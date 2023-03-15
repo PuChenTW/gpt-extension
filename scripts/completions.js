355024a7619f32773fb4a6e8b3f996db569136a4
Pu Chen <puchen.tw@gmail.com>
2023-03-15 14:49:19 +0800

    Better coding style.

--- scripts/completions.js
+++ scripts/completions.js
@@ -1,32 +1,53 @@
-var authKey = ""
+var authKey = "";
 
-chrome.storage.local.get({
-    key: '',
-}, ({ key }) => {
-    authKey = key;
-});
+chrome.storage.local.get(
+    {
+        key: "",
+    },
+    ({ key }) => {
+        authKey = key;
+    }
+);
+
+const statusMap = {
+    401: "The requesting API key is not correct. Please set the correct API key in options page.",
+    429: "The engine is currently overloaded. Retry your request later. If this issue persists, you might exceeded your current quota, please check your plan and billing details",
+    500: "Server error. Retry your request later.",
+};
 
 export function grammerCheckbyChatGPT(text, callback) {
-    fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
+    fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${authKey}`
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authKey}`,
         },
         body: JSON.stringify({
-            'model': 'gpt-3.5-turbo-0301',
-            'messages': [
-                { "role": "system", "content": "You are a grammar checker. Give user the correct sentences and suggestions." },
-                { "role": "user", "content": `Please help me to confirm the grammar of the following sentences: ${text}` }
+            model: "gpt-3.5-turbo-0301",
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        "You are a grammar checker. If the sentences have some error, give the user the correct sentences directly. Otherwise, just tell the user the grammar of the sentences is correct.",
+                },
+                { role: "user", content: `"${text}"` },
             ],
-            'max_tokens': 300,
-        })
+            max_tokens: text.length + 150,
+        }),
     })
-        .then(response => response.json())
-        .then(data => {
-            callback(data["choices"][0]["message"]["content"])
-        }).catch(error => {
-            console.error('Error:', error);
+        .then(async (response) => {
+            if (response.status !== 200) {
+                callback(
+                    statusMap?.[response.status] ??
+                    "Something went wrong, please contact dev team."
+                );
+            } else {
+                const data = await response.json();
+                callback(data?.choices?.[0]?.message?.content.trim().replace(/^"|"$/g, ""));
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            callback("Something went wrong, please contact dev team.");
         });
 }
-
--- scripts/content.js
+++ scripts/content.js
@@ -2,8 +2,12 @@ var grammerCheck = null;
 var googleTranslate = null;
 var definition = null;
 
+// Use dynamic import to import modules.
+// https://stackoverflow.com/questions/48104433/how-to-import-es6-modules-in-content-script-for-chrome-extension
 (async () => {
-    const completions = await import(chrome.runtime.getURL("scripts/completions.js"));
+    const completions = await import(
+        chrome.runtime.getURL("scripts/completions.js")
+    );
     grammerCheck = completions.grammerCheckbyChatGPT;
     definitionbyChatGPT = completions.definitionbyChatGPT;
     const translate = await import(chrome.runtime.getURL("scripts/translate.js"));
@@ -15,16 +19,14 @@ const dialogId = "gpt-ex-dialog";
 const progressSpinId = "gpt-ex-spin";
 
 function setDialogInnerText(data) {
-    const progressSpin = document.getElementById(progressSpinId)
+    const progressSpin = document.getElementById(progressSpinId);
     if (progressSpin) {
         progressSpin.style.display = "none";
     }
 
-    const dialog = document.getElementById(dialogId)
+    const dialog = document.getElementById(dialogId);
     if (dialog) {
-        dialog.classList.remove("p-4x");
-        dialog.classList.add("p-8x");
-        dialog.innerText = data.trim();
+        dialog.innerHTML = data.trim();
     }
 }
 
@@ -32,26 +34,46 @@ function createDialog(selectText, dialogTop, dialogLeft) {
     const dialog = document.createElement("div");
     dialog.id = dialogId;
     dialog.classList.add(
-        "p-4x", "max-w-sm", "bg-white", "rounded", "shadow-lg", "font-sans", "text-16x", "text-justify",
-        "flex", "items-center", "absolute", "border", "border-solid", "text-slate-800"
+        "p-4x",
+        "max-w-sm",
+        "bg-white",
+        "rounded",
+        "shadow-lg",
+        "font-sans",
+        "text-14x",
+        "flex",
+        "items-center",
+        "absolute",
+        "border",
+        "border-solid",
+        "text-slate-800"
     );
     dialog.style.zIndex = Number.MAX_SAFE_INTEGER;
     dialog.style.top = dialogTop;
     dialog.style.left = dialogLeft;
-    dialog.style.borderColor = "#BBBBBB"
-
+    dialog.style.borderColor = "#BBBBBB";
 
     const spin = document.createElement("div");
     spin.id = progressSpinId;
     spin.classList.add("items-center", "justify-center", "p-4x");
-    spin.style.display = "none"
+    spin.style.display = "none";
 
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute("viewBox", "0 0 24 24");
     svg.setAttribute("fill", "none");
-    svg.classList.add("animate-spin", "-ml-1", "mr-3", "h-5", "w-5", "text-black")
+    svg.classList.add(
+        "animate-spin",
+        "-ml-1",
+        "mr-3",
+        "h-5",
+        "w-5",
+        "text-black"
+    );
 
-    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    const circle = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle"
+    );
     circle.classList.add("opacity-25");
     circle.setAttribute("cx", "12");
     circle.setAttribute("cy", "12");
@@ -60,54 +82,82 @@ function createDialog(selectText, dialogTop, dialogLeft) {
     circle.setAttribute("stroke-width", "4");
 
     const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path.classList.add("opacity-75")
-    path.setAttribute("fill", "currentColor")
-    path.setAttribute("d", "M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z")
+    path.classList.add("opacity-75");
+    path.setAttribute("fill", "currentColor");
+    path.setAttribute(
+        "d",
+        "M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    );
 
     const innerText = document.createElement("div");
-    innerText.innerText = "Processing..."
+    innerText.innerText = "Processing...";
 
-    svg.appendChild(circle)
-    svg.appendChild(path)
+    svg.appendChild(circle);
+    svg.appendChild(path);
 
-    spin.append(svg)
-    spin.appendChild(innerText)
-
+    spin.append(svg);
+    spin.appendChild(innerText);
 
     const grammerCheckButton = document.createElement("button");
     grammerCheckButton.classList.add(
-        "p-4x", "mx-2x", "bg-lime-500", "rounded-md", "shadow-lg", "border", "border-solid", "border-slate-700", "text-white", "font-sans", "text-16x",
+        "p-4x",
+        "mx-2x",
+        "bg-lime-500",
+        "rounded-md",
+        "shadow-lg",
+        "border",
+        "border-solid",
+        "border-slate-700",
+        "text-white",
+        "font-sans",
+        "text-14x",
+        "cursor-pointer"
     );
     grammerCheckButton.innerText = "Grammer";
-    grammerCheckButton.style.borderColor = "#BBBBBB"
+    grammerCheckButton.style.borderColor = "#BBBBBB";
 
-    grammerCheckButton.addEventListener("mouseup", e => {
-        e.stopPropagation()
+    grammerCheckButton.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
         grammerCheckButton.style.display = "none";
         translateButton.style.display = "none";
         spin.style.display = "flex";
-        grammerCheck(selectText.trim().replace(/\n/g, " "), setDialogInnerText)
-    })
-
+        grammerCheck(selectText.trim().replace(/\n/g, " "), setDialogInnerText);
+    });
 
     const translateButton = document.createElement("button");
     translateButton.classList.add(
-        "p-4x", "mx-2x", "bg-cyan-500", "rounded-md", "shadow-lg", "border", "border-solid", "border-slate-700", "text-white", "font-sans", "text-16x",
+        "p-4x",
+        "mx-2x",
+        "bg-cyan-500",
+        "rounded-md",
+        "shadow-lg",
+        "border",
+        "border-solid",
+        "border-slate-700",
+        "text-white",
+        "font-sans",
+        "text-14x",
+        "cursor-pointer"
     );
     translateButton.innerText = "Translate";
-    translateButton.style.borderColor = "#BBBBBB"
+    translateButton.style.borderColor = "#BBBBBB";
 
-    translateButton.addEventListener("mouseup", e => {
-        e.stopPropagation()
+    translateButton.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
         grammerCheckButton.style.display = "none";
         translateButton.style.display = "none";
         spin.style.display = "flex";
-        googleTranslate(selectText.trim().replace(/\n/g, " "), setDialogInnerText)
-    })
+        googleTranslate(selectText.trim().replace(/\n/g, " "), setDialogInnerText);
+    });
 
-    dialog.appendChild(grammerCheckButton)
-    dialog.appendChild(translateButton)
-    dialog.appendChild(spin)
+    if (selectText.split(" ").length - 1 > 1) {
+        // If the selected text has less than 2 spaces, it probaily not a sentence.
+        dialog.appendChild(grammerCheckButton);
+    }
+    dialog.appendChild(translateButton);
+    dialog.appendChild(spin);
 
     document.body.appendChild(dialog);
 
@@ -117,31 +167,49 @@ function createDialog(selectText, dialogTop, dialogLeft) {
 function createIcon(top, left, dialogTop, dialogLeft, selectText) {
     const container = document.createElement("div");
     container.id = iconId;
-    container.classList.add("absolute", "bg-white", "shadow-lg", "rounded-md", "text-center", "border", "border-solid")
+    container.classList.add(
+        "absolute",
+        "bg-white",
+        "shadow-lg",
+        "rounded-md",
+        "text-center",
+        "border",
+        "border-solid",
+        "cursor-pointer",
+        "text-sky-400"
+    );
     container.style.width = "22px";
     container.style.height = "23px";
     container.style.top = `${top + window.scrollY + 5}px`;
     container.style.left = `${left + window.scrollX + 5}px`;
     container.style.zIndex = Number.MAX_SAFE_INTEGER;
-    container.style.borderColor = "#BBBBBB"
-
+    container.style.borderColor = "#BBBBBB";
 
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute("viewBox", "0 0 20 20");
     svg.setAttribute("fill", "currentColor");
-    svg.classList.add("h-full", "w-full", "text-sky-400")
+    svg.classList.add("h-full", "w-full", "text-sky-400");
     const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path.setAttribute("fill-rule", "evenodd")
-    path.setAttribute("d", "M10 3c-4.31 0-8 3.033-8 7 0 2.024.978 3.825 2.499 5.085a3.478 3.478 0 01-.522 1.756.75.75 0 00.584 1.143 5.976 5.976 0 003.936-1.108c.487.082.99.124 1.503.124 4.31 0 8-3.033 8-7s-3.69-7-8-7zm0 8a1 1 0 100-2 1 1 0 000 2zm-2-1a1 1 0 11-2 0 1 1 0 012 0zm5 1a1 1 0 100-2 1 1 0 000 2z")
-    path.setAttribute("clip-rule", "evenodd")
-    svg.appendChild(path)
+    path.setAttribute("fill-rule", "evenodd");
+    path.setAttribute(
+        "d",
+        "M10 3c-4.31 0-8 3.033-8 7 0 2.024.978 3.825 2.499 5.085a3.478 3.478 0 01-.522 1.756.75.75 0 00.584 1.143 5.976 5.976 0 003.936-1.108c.487.082.99.124 1.503.124 4.31 0 8-3.033 8-7s-3.69-7-8-7zm0 8a1 1 0 100-2 1 1 0 000 2zm-2-1a1 1 0 11-2 0 1 1 0 012 0zm5 1a1 1 0 100-2 1 1 0 000 2z"
+    );
+    path.setAttribute("clip-rule", "evenodd");
+    svg.appendChild(path);
+
+    container.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    });
 
     container.addEventListener("mouseup", (e) => {
+        e.preventDefault();
         e.stopPropagation();
         createDialog(selectText, dialogTop, dialogLeft);
         removeIcon();
     });
-    container.appendChild(svg)
+    container.appendChild(svg);
     document.body.appendChild(container);
     return container;
 }
@@ -160,13 +228,14 @@ function removeDialog() {
     }
 }
 
-function showDialog(e) {
+function showIcon(e) {
     const selection = window.getSelection();
     const text = selection.toString().trim();
     if (
         !document.getElementById(iconId) &&
         !document.getElementById(dialogId) &&
-        text.length > 0 && text.length < 250
+        text.length > 0 &&
+        text.length < 250
     ) {
         const range = selection.getRangeAt(0);
         const dialogTop = `${range.getBoundingClientRect().bottom + window.scrollY + 5
@@ -174,14 +243,42 @@ function showDialog(e) {
         const dialogLeft = `${range.getBoundingClientRect().left + window.scrollX
             }px`;
         createIcon(e.clientY, e.clientX, dialogTop, dialogLeft, text);
-    } else {
-        removeIcon();
-        if (e.target && e.target.id != dialogId) {
-            removeDialog();
-        }
     }
 }
 
+function clickInDialog(e) {
+    const rect = document.getElementById(dialogId)?.getBoundingClientRect();
+    if (!rect) return false;
+
+    return (
+        e.clientX >= rect.left &&
+        e.clientX <= rect.right &&
+        e.clientY >= rect.top &&
+        e.clientY <= rect.bottom
+    );
+}
+
+function clickInIcon(e) {
+    const rect = document.getElementById(iconId)?.getBoundingClientRect();
+    if (!rect) return false;
+
+    return (
+        e.clientX >= rect.left &&
+        e.clientX <= rect.right &&
+        e.clientY >= rect.top &&
+        e.clientY <= rect.bottom
+    );
+}
+
 document.addEventListener("mouseup", (e) => {
-    showDialog(e);
+    showIcon(e);
+});
+
+document.addEventListener("mousedown", (e) => {
+    if (!clickInDialog(e)) {
+        removeDialog();
+    }
+    if (!clickInIcon(e)) {
+        removeIcon();
+    }
 });
