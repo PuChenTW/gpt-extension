06731fa29a7852d2a0d283cb5569b30efd701b7b
Pu Chen <puchen.tw@gmail.com>
2023-04-26 00:43:17 +0800

    Add tab in options.

--- react-app/src/options/App.tsx
+++ react-app/src/options/App.tsx
@@ -1,7 +1,13 @@
+
+import { TabView, TabPanel} from 'primereact/tabview';
+
 import { PromptInput } from "./promptInput"
 import { KeyInput } from "./keyInput"
 import { ModelInput } from "./modelInput"
 import { LanguageInput } from "./languageInput"
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 
 interface labelName {
     labelName: string;
@@ -17,17 +23,34 @@ function Label({ labelName }: labelName) {
 
 function App() {
     return (
-        <div className="cs-flex cs-h-screen cs-items-center cs-justify-center">
-            <div className="cs-grid cs-grid-cols-2 cs-items-center cs-gap-4">
-                <Label labelName="Primary Language"></Label>
-                <LanguageInput />
-                <Label labelName="Your ChatGPT API Key"></Label>
-                <KeyInput />
-                <Label labelName="Model"></Label>
-                <ModelInput />
-                <Label labelName="Prompt"></Label>
-                <PromptInput />
-            </div>
+        <div className="cs-flex cs-h-screen cs-w-screen cs-items-center cs-justify-center">
+            <TabView className='cs-w-1/3 cs-h-1/3'>
+                <TabPanel
+                    header="General"
+                    prevButton={undefined}
+                    nextButton={undefined}
+                    closeIcon={undefined}
+                >
+                    <div className="cs-grid cs-grid-cols-2 cs-items-center cs-gap-4">
+                        <Label labelName="Primary Language"></Label>
+                        <LanguageInput />
+                        <Label labelName="Your ChatGPT API Key"></Label>
+                        <KeyInput />
+                        <Label labelName="Model"></Label>
+                        <ModelInput />
+                    </div>
+                </TabPanel>
+                <TabPanel
+                    header="Prompt I"
+                    prevButton={undefined}
+                    nextButton={undefined}
+                    closeIcon={undefined}
+                >
+                    <div className="cs-flex cs-justify-center">
+                        <PromptInput />
+                    </div>
+                </TabPanel>
+            </TabView>
         </div>
     );
 }
--- react-app/src/options/promptInput.tsx
+++ react-app/src/options/promptInput.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect, useCallback, ChangeEvent } from "react";
 
-const DefaultPrompt: string = `You are a grammar checker, if there are errors, provide the correct sentences and suggestions. Please check the following sentences:\n"""\n{{text}}\n"""`;
+const GrammerPrompt: string = `You are a grammar checker, if there are errors, provide the correct sentences and suggestions. Please check the following sentences:\n"""\n{{text}}\n"""`;
+const SummaryPrompt: string = `Summarize the following article:\n"""\n{{text}}\n"""`;
 
 export function PromptInput() {
-    const [prompt, setPrompt] = useState(DefaultPrompt);
+    const [prompt, setPrompt] = useState(GrammerPrompt);
     const onChangePrompt = useCallback(
         (e: ChangeEvent<HTMLTextAreaElement>) => {
             setPrompt(e.target.value);
@@ -12,15 +13,20 @@ export function PromptInput() {
         [setPrompt]
     );
 
-    const onResetClick = useCallback(() => {
-        setPrompt(DefaultPrompt);
-        chrome.storage.local.set({ prompt: DefaultPrompt });
+    const onGrammerClick = useCallback(() => {
+        setPrompt(GrammerPrompt);
+        chrome.storage.local.set({ prompt: GrammerPrompt });
+    }, [setPrompt]);
+
+    const onSummaryClick = useCallback(() => {
+        setPrompt(SummaryPrompt);
+        chrome.storage.local.set({ prompt: SummaryPrompt });
     }, [setPrompt]);
 
     useEffect(() => {
         chrome.storage.local.get(
             {
-                prompt: DefaultPrompt,
+                prompt: GrammerPrompt,
             },
             ({ prompt }) => {
                 setPrompt(prompt);
@@ -39,11 +45,18 @@ export function PromptInput() {
                 {"{{text}} will be replaced with selected text"}
             </p>
             <button
-                id="reset"
+                id="grammer"
                 className="cs-my-2 cs-bg-blue-500 hover:cs-bg-blue-700 cs-text-white cs-font-bold cs-py-2 cs-px-4 cs-rounded"
-                onClick={onResetClick}
+                onClick={onGrammerClick}
             >
-                Reset
+                Grammer
+            </button>
+            <button
+                id="grammer"
+                className="cs-my-2 cs-mx-2 cs-bg-green-500 hover:cs-bg-green-700 cs-text-white cs-font-bold cs-py-2 cs-px-4 cs-rounded"
+                onClick={onSummaryClick}
+            >
+                Summary
             </button>
         </div>
     );
