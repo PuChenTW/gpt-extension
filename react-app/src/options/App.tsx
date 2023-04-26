769793d2ff807ea4c67686058d3165e1f32f771e
Pu Chen <puchen.tw@gmail.com>
2023-04-26 13:01:43 +0800

    Use list to store prompt.

--- react-app/src/options/App.tsx
+++ react-app/src/options/App.tsx
@@ -1,7 +1,9 @@
 
+import { useCallback, useEffect } from 'react';
+import { useImmer } from "use-immer";
 import { TabView, TabPanel} from 'primereact/tabview';
 
-import { PromptInput } from "./promptInput"
+import { PromptInput, GrammerPrompt } from "./promptInput"
 import { KeyInput } from "./keyInput"
 import { ModelInput } from "./modelInput"
 import { LanguageInput } from "./languageInput"
@@ -21,7 +23,37 @@ function Label({ labelName }: labelName) {
     );
 }
 
+interface promptObject {
+    header: string
+    prompt: string
+}
+
 function App() {
+
+    const [prompts, updatePrompts] = useImmer<promptObject[]>([])
+
+    useEffect(() => {
+        chrome.storage.local.get(
+            {
+                prompts: [{header: "Grammer", prompt: GrammerPrompt}],
+            },
+            ({ prompts }) => {
+                console.log(prompts)
+                updatePrompts(prompts);
+            }
+        );
+    }, [updatePrompts])
+
+    useEffect(() => {
+        chrome.storage.local.set({prompts})
+    }, [prompts])
+
+    const onChangePrompt = useCallback((prompt: string, idx: number) => {
+        updatePrompts(prompts => {
+            prompts[idx].prompt = prompt
+        })
+    }, [updatePrompts])
+
     return (
         <div className="cs-flex cs-h-screen cs-w-screen cs-items-center cs-justify-center">
             <TabView className='cs-w-1/3 cs-h-1/3'>
@@ -32,7 +64,7 @@ function App() {
                     closeIcon={undefined}
                 >
                     <div className="cs-grid cs-grid-cols-2 cs-items-center cs-gap-4">
-                        <Label labelName="Primary Language"></Label>
+                        <Label labelName="Primary Language (Google translate)"></Label>
                         <LanguageInput />
                         <Label labelName="Your ChatGPT API Key"></Label>
                         <KeyInput />
@@ -40,16 +72,20 @@ function App() {
                         <ModelInput />
                     </div>
                 </TabPanel>
-                <TabPanel
-                    header="Prompt I"
-                    prevButton={undefined}
-                    nextButton={undefined}
-                    closeIcon={undefined}
-                >
-                    <div className="cs-flex cs-justify-center">
-                        <PromptInput />
-                    </div>
-                </TabPanel>
+                {prompts.map(({header, prompt}, idx) => (
+                    <TabPanel
+                        header={header}
+                        prevButton={undefined}
+                        nextButton={undefined}
+                        closeIcon={undefined}
+                        key={`${header}_${idx}`}
+                    >
+                        <PromptInput 
+                            prompt={prompt}
+                            onChange={(prompt: string) => {onChangePrompt(prompt, idx)}}
+                        />
+                    </TabPanel>
+                ))}
             </TabView>
         </div>
     );
--- react-app/src/options/promptInput.tsx
+++ react-app/src/options/promptInput.tsx
@@ -1,63 +1,55 @@
-import { useState, useEffect, useCallback, ChangeEvent } from "react";
+import { useState, useCallback, ChangeEvent } from "react";
 
-const GrammerPrompt: string = `You are a grammar checker, if there are errors, provide the correct sentences and suggestions. Please check the following sentences:\n"""\n{{text}}\n"""`;
+export const GrammerPrompt: string = `You are a grammar checker, if there are errors, provide the correct sentences and suggestions. Please check the following sentences:\n"""\n{{text}}\n"""`;
 const SummaryPrompt: string = `Summarize the following article:\n"""\n{{text}}\n"""`;
 
-export function PromptInput() {
-    const [prompt, setPrompt] = useState(GrammerPrompt);
+export function PromptInput({prompt, onChange}: {prompt: string, onChange: Function}) {
+    const [localPrompt, setLocalPrompt] = useState(prompt);
     const onChangePrompt = useCallback(
         (e: ChangeEvent<HTMLTextAreaElement>) => {
-            setPrompt(e.target.value);
-            chrome.storage.local.set({ prompt: e.target.value });
+            setLocalPrompt(e.target.value);
+            onChange(e.target.value)
         },
-        [setPrompt]
+        [setLocalPrompt]
     );
 
     const onGrammerClick = useCallback(() => {
-        setPrompt(GrammerPrompt);
-        chrome.storage.local.set({ prompt: GrammerPrompt });
-    }, [setPrompt]);
+        setLocalPrompt(GrammerPrompt);
+        onChange(GrammerPrompt)
+    }, [setLocalPrompt]);
 
     const onSummaryClick = useCallback(() => {
-        setPrompt(SummaryPrompt);
-        chrome.storage.local.set({ prompt: SummaryPrompt });
-    }, [setPrompt]);
+        setLocalPrompt(SummaryPrompt);
+        onChange(SummaryPrompt)
+    }, [setLocalPrompt]);
 
-    useEffect(() => {
-        chrome.storage.local.get(
-            {
-                prompt: GrammerPrompt,
-            },
-            ({ prompt }) => {
-                setPrompt(prompt);
-            }
-        );
-    }, [setPrompt]);
     return (
-        <div>
+        <div className="cs-flex cs-flex-col cs-items-center">
             <textarea
                 id="prompt"
                 className="cs-w-80 cs-block cs-text-sm cs-p-2 cs-rounded-md cs-border cs-border-gray-300 cs-h-40"
                 onInput={onChangePrompt}
-                value={prompt}
+                value={localPrompt}
             />
             <p className="cs-font-sans cs-text-teal-800">
                 {"{{text}} will be replaced with selected text"}
             </p>
-            <button
-                id="grammer"
-                className="cs-my-2 cs-bg-blue-500 hover:cs-bg-blue-700 cs-text-white cs-font-bold cs-py-2 cs-px-4 cs-rounded"
-                onClick={onGrammerClick}
-            >
-                Grammer
-            </button>
-            <button
-                id="grammer"
-                className="cs-my-2 cs-mx-2 cs-bg-green-500 hover:cs-bg-green-700 cs-text-white cs-font-bold cs-py-2 cs-px-4 cs-rounded"
-                onClick={onSummaryClick}
-            >
-                Summary
-            </button>
+            <div>
+                <button
+                    id="grammer"
+                    className="cs-my-2 cs-bg-blue-500 hover:cs-bg-blue-700 cs-text-white cs-font-bold cs-py-2 cs-px-4 cs-rounded"
+                    onClick={onGrammerClick}
+                >
+                    Grammer
+                </button>
+                <button
+                    id="grammer"
+                    className="cs-my-2 cs-mx-2 cs-bg-green-500 hover:cs-bg-green-700 cs-text-white cs-font-bold cs-py-2 cs-px-4 cs-rounded"
+                    onClick={onSummaryClick}
+                >
+                    Summary
+                </button>
+            </div>
         </div>
     );
 }
