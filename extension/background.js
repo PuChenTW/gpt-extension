a44cc77dcad22c41050cf87523bea0c128f0f365
Pu Chen <puchen.tw@gmail.com>
2024-10-04 22:43:20 +0800

    Open option page by click icon.

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,11 @@
+chrome.action.onClicked.addListener((tab) => {
+  chrome.runtime.openOptionsPage();
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.get({
     key: '',
   }, ({ key }) => {
-      if (!key) chrome.runtime.openOptionsPage();
+    if (!key) chrome.runtime.openOptionsPage();
   });
 });
