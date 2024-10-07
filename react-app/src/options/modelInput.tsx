0ac2c4d0f45b3e9048c50d34aacd070deab6c43e
Pu Chen <puchen.tw@gmail.com>
2024-10-07 10:48:58 +0800

    Add gpt-4o in model options.

--- react-app/src/options/modelInput.tsx
+++ react-app/src/options/modelInput.tsx
@@ -1,13 +1,10 @@
 import { useState, useEffect, useCallback, ChangeEvent } from "react";
 
 const modelOptions = [
+    { value: "gpt-4o", label: "gpt-4o" },
     { value: "gpt-4", label: "gpt-4" },
     { value: "gpt-4-0613", label: "gpt-4-0613" },
     { value: "gpt-4-32k-0613", label: "gpt-4-32k-0613" },
-    { value: "gpt-3.5-turbo", label: "gpt-3.5-turbo" },
-    { value: "gpt-3.5-turbo-0613", label: "gpt-3.5-turbo-0613" },
-    { value: "gpt-3.5-turbo-16k", label: "gpt-3.5-turbo-16k" },
-    { value: "gpt-3.5-turbo-16k-0613", label: "gpt-3.5-turbo-16k-0613" },
 ];
 
 const defaultModel = "gpt-3.5-turbo"
